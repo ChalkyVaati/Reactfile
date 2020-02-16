c9ac83094ec7dde050b264ddafcb050dc2a3e2a3
ChalkyVaati <35500125+ChalkyVaati@users.noreply.github.com>
2020-02-16 17:38:43 -0500

    Initial commit

---
+++ src/MyApp.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function MyApp() {
+  return (
+    <div>
+      <div>
+        <h1>Look at me learning a new language</h1>
+      </div>
+      <div>
+        <p>
+          Hello Alyza I have been typing and learning React code by myself
+          because I have a very adaptable brain when it comes to react
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default MyApp;
